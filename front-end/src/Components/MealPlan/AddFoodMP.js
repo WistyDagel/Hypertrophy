31028b8a1d6cc4066316a605d71132fa817e376b
WistyDagel <chrisdazley@gmail.com>
2020-11-17 10:29:27 -0700

    Update AddFoodMP.js

--- front-end/src/Components/MealPlan/AddFoodMP.js
+++ front-end/src/Components/MealPlan/AddFoodMP.js
@@ -9,7 +9,8 @@ class AddFoodMP extends Component {
 
     //API EXAMPLE FOR FOOD DB SEARCH AND NUTRITION FACTS
     //https://api.nutritionix.com/v1_1/search/mcdonalds?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=a26ca4ac&appKey=020c2cb566c8fabe83c4819e737fae18
-
+    //https://api.nutritionix.com/v1_1/search/mcdonalds?results=0:20&fields=nf_calories,nf_protein,nf_total_carbohydrate,nf_total_fat,nf_sugars&appId=a26ca4ac&appKey=020c2cb566c8fabe83c4819e737fae18
+    
     render() {
         return  (
             <>
