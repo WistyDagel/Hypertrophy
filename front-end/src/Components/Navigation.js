e4d8c2cb22f263695803a6200127c98ae63e7b91
WistyDagel <chrisdazley@gmail.com>
2020-11-29 11:19:40 -0700

    Added navbar icons

--- front-end/src/Components/Navigation.js
+++ front-end/src/Components/Navigation.js
@@ -1,6 +1,10 @@
-import React, { Component } from 'react';
- 
+import React, { Component } from 'react'; 
 import { Link, NavLink } from 'react-router-dom';
+import home from './Photos/home.png'
+import log from './Photos/log.png'
+import apple from './Photos/apple.png'
+import dumbbell from './Photos/dumbbell.png'
+
  
 
 class Navigation extends Component {
@@ -11,37 +15,37 @@ class Navigation extends Component {
     render(){
         return (
             <div className="navbar">
-                <div className="button">
+                <div className="button col">
+                    <img style={{width: "20px", height: "20px"}} src={home}></img>
                     <Link
                         to={{
                             pathname: '/home',
                         }}
                     >Home</Link>
-                    {/* <a href="/home">Home</a> */}
                 </div>
-                <div className="button">
+                <div className="button col">
+                    <img style={{width: "20px", height: "20px"}} src={log}></img>
                     <Link
                         to={{
                             pathname: '/fitnesslog',
                         }}
                     >Log</Link>
-                    {/* <a href="/fitnesslog">Log</a> */}
                 </div>
-                <div className="button">
+                <div className="button col">
+                    <img style={{width: "20px", height: "20px"}} src={apple}></img>
                     <Link
                         to={{
                             pathname: '/mealhome',
                         }}
                     >Diet</Link>
-                    {/* <a href="/mealhome">Diet</a> */}
                 </div>
-                <div className="button">
+                <div className="button col">
+                    <img style={{width: "20px", height: "20px"}} src={dumbbell}></img>
                     <Link
                         to={{
                             pathname: '/workouthome',
                         }}
                     >Exercise</Link>
-                    {/* <a href="/workouthome">Exercise</a> */}
                 </div>
             </div>
         );
